4d7511ea4d4776ab76e86249c3dac1eb94489820
Patrick Hamers <phamers@ziggo.nl>
2021-06-16 09:58:51 +0200

    Change values of FeeCalculationStrategy

--- src/ProximaX.Sirius.Chain.Sdk/Model/Fees/FeeCalculationStrategyType.cs
+++ src/ProximaX.Sirius.Chain.Sdk/Model/Fees/FeeCalculationStrategyType.cs
@@ -17,8 +17,8 @@ namespace ProximaX.Sirius.Chain.Sdk.Model.Fees
     public enum FeeCalculationStrategyType : int
     {
         ZERO = 0,
-        LOW = 25,
-        MEDIUM = 250,
-        HIGH = 2500
+        LOW = 15000,
+        MEDIUM = 150000,
+        HIGH = 1500000
     }
 }
