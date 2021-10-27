764cb89c1b8f954f4f84270f3b19377d8f91fa3e
haieng1234 <haieng.chong@proximax.io>
2021-10-27 18:52:07 +0800

    mosaic levy model

---
+++ src/ProximaX.Sirius.Chain.Sdk/Model/Mosaics/MosaicLevyInfo.cs
@@ -0,0 +1,61 @@
+﻿// Copyright 2021 ProximaX
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using ProximaX.Sirius.Chain.Sdk.Model.Accounts;
+using ProximaX.Sirius.Chain.Sdk.Model.Namespaces;
+using ProximaX.Sirius.Chain.Sdk.Model.Transactions;
+using ProximaX.Sirius.Chain.Sdk.Utils;
+
+namespace ProximaX.Sirius.Chain.Sdk.Model.Mosaics
+{
+    /// <summary>
+    ///     The mosaic name info structure describes basic information of a mosaic and name.
+    /// </summary>
+    public class MosaicLevyInfo
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Mosaic" /> class.
+        /// </summary>
+        /// <param name="id">The mosaic identifier</param>
+        /// <param name="amount">The mosaic account</param>
+        public MosaicLevyInfo(MosaicLevyType levytype, Recipient recipent, MosaicId mosaicid, ulong fee)
+        {
+            Levytype = levytype;
+            Recipent = recipent;
+            Mosaic = mosaicid;
+            Fee = fee;
+        }
+
+        public MosaicLevyType Levytype { get; }
+        public Recipient Recipent { get; }
+        public MosaicId Mosaic { get; }
+        public ulong Fee { get; }
+
+        public static MosaicLevyInfo Create(MosaicLevyType levytype, Recipient recipent, MosaicId mosaicid, ulong fee)
+        {
+            return new MosaicLevyInfo(levytype, recipent, mosaicid, fee);
+        }
+
+        public static ulong CreateMosaicLevyFeePercentile(float percent)
+        {
+            int MosaicLevyDecimalPlace = 100000;
+            return ((ulong)(percent * MosaicLevyDecimalPlace));
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Levytype)}: {Levytype}, {nameof(Recipent)}: {Recipent}, {nameof(Mosaic)}: {Mosaic}, {nameof(Fee)}: {Fee}";
+        }
+    }
+}
---
+++ src/ProximaX.Sirius.Chain.Sdk/Model/Mosaics/MosaicRichList.cs
@@ -0,0 +1,70 @@
+﻿// Copyright 2019 ProximaX
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using ProximaX.Sirius.Chain.Sdk.Model.Accounts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ProximaX.Sirius.Chain.Sdk.Model.Mosaics
+{
+    /// <summary>
+    ///     MosaicInfo
+    /// </summary>
+    public class MosaicRichList
+    {
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        /// <param name="address">The address</param>
+        /// <param name="publicKey">The publicKey</param>
+        /// <param name="amount">The mosaic supply</param>
+
+        public MosaicRichList(Address address, string? publicKey, ulong amount)
+                   // public MosaicRichList(Address address, string? publicKey)
+
+        {
+            Address = address;
+            PublicKey = publicKey;
+            Amount = amount;
+        }
+
+        /// <summary>
+        ///     The Address
+        /// </summary>
+        public Address Address { get; }
+
+        /// <summary>
+        ///     The PublicKey
+        /// </summary>
+        public string PublicKey { get; }
+
+        /// <summary>
+        ///     The Amount
+        /// </summary>
+        public ulong Amount { get; }
+
+
+
+       public override string ToString()
+        {
+            return $"{nameof(Address)}: {Address}, {nameof(PublicKey)}: {PublicKey}, {nameof(Amount)}: {Amount}";
+
+            //return $"{nameof(Address)}: {Address}, {nameof(PublicKey)}: {PublicKey}";
+
+        }
+
+
+    }
+}
