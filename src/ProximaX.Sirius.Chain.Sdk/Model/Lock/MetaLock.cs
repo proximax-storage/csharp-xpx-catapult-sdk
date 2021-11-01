0eaa223b849c2ed384d2428d85e855a55432de29
haieng1234 <haieng.chong@proximax.io>
2021-11-01 15:45:26 +0800

    Missing code - Lock

---
+++ src/ProximaX.Sirius.Chain.Sdk/Model/Lock/MetaLock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProximaX.Sirius.Chain.Sdk.Model.Lock
+{
+    public class MetaLock
+    {
+        public MetaLock(string id)
+        {
+            Id = id;
+        }
+        public string Id { get; }
+
+    }
+}
