4aab57c8fffb64c989bba96c1589a48d9cc9622b
Hai Eng <haieng.chong@proximax.io>
2021-12-07 17:08:48 +0800

    typo on queryparam

--- src/ProximaX.Sirius.Chain.Sdk/Infrastructure/BlockHttp.cs
+++ src/ProximaX.Sirius.Chain.Sdk/Infrastructure/BlockHttp.cs
@@ -211,7 +211,7 @@ namespace ProximaX.Sirius.Chain.Sdk.Infrastructure
                 switch (query.Order)
                 {
                     case Order.ASC:
-                        route = route.SetQueryParam("ordering", "-id");
+                        route = route.SetQueryParam("ordering", "id");
                         route = route.SetQueryParam("block", "meta.height");
 
                         break;
